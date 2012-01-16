406d6b30647a08115fb4d68d46efa9409040045a
Hugo Parente Lima <hugo.lima@openbossa.org>
2012-01-16 15:13:42 -0200

    Use QML booster only when compiling to Harmattan target.

--- luna/main.cpp
+++ luna/main.cpp
@@ -1,11 +1,19 @@
 #include <QApplication>
 #include <QDeclarativeView>
 #include <QUrl>
-#include <MDeclarativeCache>
+#include <qplatformdefs.h>
 
-Q_DECL_EXPORT int main(int argc, char** argv)
+#if defined(MEEGO_EDITION_HARMATTAN)
+    #include <MDeclarativeCache>
+    #define NEW_QAPPLICATION(x, y) MDeclarativeCache::qApplication((x), (y))
+    Q_DECL_EXPORT
+#else
+    #define NEW_QAPPLICATION(x, y) new QApplication((x), (y))
+#endif
+
+int main(int argc, char** argv)
 {
-    QApplication* app = MDeclarativeCache::qApplication(argc, argv);
+    QApplication* app = NEW_QAPPLICATION(argc, argv);
 
     QDeclarativeView viewer;
     viewer.setSource(QUrl("qrc:/qml/main.qml"));
